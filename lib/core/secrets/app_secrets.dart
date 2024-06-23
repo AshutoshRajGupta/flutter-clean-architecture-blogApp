3b6db8875aa48f317cae952ecf1d62c10214ad89
Ashutosh Raj Gupta <ag2364443@gmail.com>
2024-06-23 12:39:07 +0530

    completed

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://xzhghuytlprugwxumevx.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6aGdodXl0bHBydWd3eHVtZXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg4MDY1MDQsImV4cCI6MjAzNDM4MjUwNH0.z4SecGYde8CNyhxl5K-649qc2FzMPvMmAnLlmr5QVtQ';
+}
---
+++ lib/features/blog/data/datasources/blog_remote_data_source.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:blog_app/features/blog/data/models/blog_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class BlogRemoteDataSource {
+  Future<BlogModel> uploadBlog(BlogModel blog);
+  Future<String> uploadBlogImage({
+    required File image,
+    // we have id in blogmodel which is required for uploading image
+    required BlogModel blog,
+  });
+  Future<List<BlogModel>> getAllBlogs();
+}
+
+class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
+  final SupabaseClient supabaseClient;
+  BlogRemoteDataSourceImpl(this.supabaseClient);
+
+  @override
+  Future<BlogModel> uploadBlog(BlogModel blog) async {
+    try {
+      // here we are uplaoding the data to our database
+      final blogData =
+          await supabaseClient.from('blogs').insert(blog.toJson()).select();
+
+// and here it will return the data
+      return BlogModel.fromJson(blogData.first);
+    } on PostgrestException catch (e) {
+      throw ServerException(e.message);
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+
+  @override
+  Future<String> uploadBlogImage({
+    required File image,
+    required BlogModel blog,
+  }) async {
+    try {
+      // here we are created image bucket and here we are uploading image in the buckey on supabase
+      await supabaseClient.storage.from('blog_images').upload(
+            blog.id,
+            image,
+          );
+// and here we are getting the url for the imaege
+      return supabaseClient.storage.from('blog_images').getPublicUrl(
+            blog.id,
+          );
+    } on StorageException catch (e) {
+      throw ServerException(e.message);
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+
+  @override
+  Future<List<BlogModel>> getAllBlogs() async {
+    try {
+      final blogs =
+          await supabaseClient.from('blogs').select('*, profiles (name)');
+      return blogs
+          .map(
+            (blog) => BlogModel.fromJson(blog).copyWith(
+              posterName: blog['profiles']['name'],
+            ),
+          )
+          .toList();
+    } on PostgrestException catch (e) {
+      throw ServerException(e.message);
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+}
